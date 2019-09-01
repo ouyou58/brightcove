f2d1bae267c8fe255ff9756d65183a582a363087
ouyou58 <you@yourdomain.come>
2019-09-01 20:18:23 +0900

    Initial Commit

---
+++ Brightcove/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Brightcove
+//
+//  Created by ouyou on 2019/09/01.
+//  Copyright © 2019 ouyou. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
